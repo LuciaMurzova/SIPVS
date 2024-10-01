44b04d0f2a61df0eb87f91a5b99e564f6123ae37
Matej Lednický <lednicky@alfabase.sk>
2024-10-01 09:39:46 +0200

    index

--- src/main/java/fiit/sipvs/zadanie/controllers/FormController.java
+++ src/main/java/fiit/sipvs/zadanie/controllers/FormController.java
@@ -1,11 +1,15 @@
 package fiit.sipvs.zadanie.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FormController {
 
-    
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
 
 }
